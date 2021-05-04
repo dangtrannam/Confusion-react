682b2fdead8a76c69d247bdb4939ebfea15c8d4e
dangtrannam <dangtrannam2001@gmail.com>
2021-05-05 01:23:37 +0700

    Fucntional Components

--- confusion/src/components/Dishdetail.js
+++ confusion/src/components/Dishdetail.js
@@ -1,76 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
 
-export default class Dishdetail extends Component {
 
-    renderDish(dish) {
-        return (
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
+const RenderDish = ({ dish }) => {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+};
+
+const RenderComments = ({ dish }) => {
+
+    let comments = dish.comments;
+
+    function convertDateToCommentDateFormat(timestamp) {
+        const date = new Date(Date.parse(timestamp));
+        return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
     }
 
-    renderComments(comments) {
-
-        function convertDateToCommentDateFormat(timestamp) {
-            const date = new Date(Date.parse(timestamp));
-            return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
-        }
-
-        let renderedComments;
-        if (comments == null || comments.length === 0) {
-            return (
-                <div></div>
-            );
-        } else {
-            renderedComments = comments.map((comment) => {
-                return (
-                    <li>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {convertDateToCommentDateFormat(comment.date)}</p>
-                    </li>
-                );
-            });
-        }
-
-
+    let renderedComments;
+    if (comments == null || comments.length === 0) {
         return (
-            <div>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {renderedComments}
-                </ul>
+            <div></div>
+        );
+    } else {
+        renderedComments = comments.map((comment) => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {convertDateToCommentDateFormat(comment.date)}</p>
+                </li>
+            );
+        });
+    }
+
+    return (
+        <div>
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {renderedComments}
+            </ul>
+        </div>
+    );
+};
+
+const DishDeltail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className='row'>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments dish={props.dish} />
+                    </div>
+                </div>
             </div>
         );
     }
-
-    render() {
-        if (this.props.dish != null) {
-            return (
-                <div className="container">
-                    <div className='row'>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
-                        </div>
-
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    else {
+        return (
+            <div></div>
+        );
     }
-
 }
+
+export default DishDeltail;
+
